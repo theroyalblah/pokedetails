ca7bf05c8f7322e1850882b10641780c886249b8
Simon Barber <theroyalblah@gmail.com>
2022-07-25 02:16:50 -0400

    vgc stats and types

--- pages/[pokemon].tsx
+++ pages/[pokemon].tsx
@@ -10,14 +10,19 @@ import Search from "../components/search";
 import { Smogon } from "@pkmn/smogon";
 import { Generations } from "@pkmn/data";
 import { Dex } from "@pkmn/dex";
-import pokemonList from '../utils/allpokemon';
+import pokemonList from "../utils/allpokemon";
 
 type PokeDetailsProps = {
   data: Pokedex.Pokemon | string | undefined;
   smogonStats: SmogonStats;
+  vgcStats: SmogonStats;
 };
 
-const PokeDetails = ({ data, smogonStats }: PokeDetailsProps) => {
+const capitalize = (s: string) => {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+const PokeDetails = ({ data, smogonStats, vgcStats }: PokeDetailsProps) => {
   if (!data || typeof data === "string") {
     return (
       <>
@@ -27,16 +32,28 @@ const PokeDetails = ({ data, smogonStats }: PokeDetailsProps) => {
     );
   }
 
-  const { name, stats } = data;
+  const { name, stats, types } = data;
 
   return (
     <main className="poke-details">
       <Container>
         <Search />
         <h1>{capFirstLetter(name)}</h1>
+
+        {types.map((type) => {
+          return (
+            <span key={type.type.name} className="types">
+              <span className={`types__${type.type.name}`}>
+                {capitalize(type.type.name)}
+              </span>
+            </span>
+          );
+        })}
+
         <Row>
           <Col sm={3}>
             <h2>Sprites</h2>
+
             {data.sprites.front_default ? (
               <img
                 width={120}
@@ -45,6 +62,7 @@ const PokeDetails = ({ data, smogonStats }: PokeDetailsProps) => {
                 src={data.sprites.front_default}
               />
             ) : null}
+
             {data.sprites.front_shiny ? (
               <img
                 width={120}
@@ -53,6 +71,7 @@ const PokeDetails = ({ data, smogonStats }: PokeDetailsProps) => {
                 src={data.sprites.front_shiny ?? ""}
               />
             ) : null}
+
             {data.sprites.back_default ? (
               <img
                 width={120}
@@ -61,6 +80,7 @@ const PokeDetails = ({ data, smogonStats }: PokeDetailsProps) => {
                 src={data.sprites.back_default ?? ""}
               />
             ) : null}
+
             {data.sprites.back_shiny ? (
               <img
                 width={120}
@@ -70,17 +90,38 @@ const PokeDetails = ({ data, smogonStats }: PokeDetailsProps) => {
               />
             ) : null}
           </Col>
+
           <Col sm={9}>
             <StatsChart stats={stats} />
           </Col>
         </Row>
+
         <Row>
           {!smogonStats?.error ? (
-            <UsageDetails {...smogonStats} />
+            <>
+              <h2>Gen 8 OU</h2>
+
+              <UsageDetails {...smogonStats} />
+            </>
           ) : (
             <p>Looks like there&lsquo;s no usage in gen 8 OU :(</p>
           )}
         </Row>
+
+        {vgcStats && (
+          <>
+            <h2>VGC 2022 Stats</h2>
+
+            <Row>
+              {!vgcStats?.error ? (
+                <UsageDetails {...vgcStats} />
+              ) : (
+                <p>Looks like there&lsquo;s no usage in vgc :(</p>
+              )}
+            </Row>
+          </>
+        )}
+
         <Search />
       </Container>
     </main>
@@ -88,15 +129,26 @@ const PokeDetails = ({ data, smogonStats }: PokeDetailsProps) => {
 };
 
 export const getStaticProps: GetServerSideProps = async ({ params }) => {
-  const pokemonName = params?.pokemon ?? '';
+  const pokemonName = params?.pokemon ?? "";
   const pokemon = (pokemonName as string).toLowerCase();
   const P = new Pokedex();
 
   const gens = new Generations(Dex);
   const smogon = new Smogon(fetch);
 
-  const pokedata = await P.getPokemonByName(pokemon);
+  let pokedata;
+  try {
+    pokedata = await P.getPokemonByName(pokemon);
+  } catch (e) {}
+
   const smogonStats = await smogon.stats(gens.get(8), pokemon);
+
+  // smogon's wonky typing is the issue behind this any type, a string is desirable here
+  let vgcStats = await smogon.stats(
+    gens.get(8),
+    pokemon,
+    "gen8vgc2022" as any
+  );
 
   if (!pokedata) {
     return {
@@ -106,30 +158,24 @@ export const getStaticProps: GetServerSideProps = async ({ params }) => {
     };
   }
 
-  if (pokedata && !smogonStats) {
-    return {
-      props: {
-        data: pokedata,
-        smogonStats: { error: `This pokemon isn't used in gen 8 ou :( ` },
-      },
-    };
-  }
   return {
     props: {
       data: pokedata,
-      smogonStats: smogonStats,
+      smogonStats: smogonStats ?? { error: `This pokemon isn't used in gen 8 ou :( ` },
+      vgcStats: vgcStats ?? { error: `This pokemon isn't used in vgc :( ` },
     },
   };
 };
 
 export async function getStaticPaths() {
   const pokemonPaths = pokemonList.map((poke) => {
-    return `/${poke.toLowerCase().replace(' ', '')}`
-  })
+    return `/${poke.toLowerCase().replace(" ", "")}`;
+  });
+
   return {
     paths: pokemonPaths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export default PokeDetails;
