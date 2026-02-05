1e32e43836d96564164f383a727ddf168af1d0f6
Simon Barber <simon@mojotech.com>
2026-02-05 15:43:37 -0500

    dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,30 @@ import "../styles/pokemon.scss";
 import "../styles/searchBar.scss";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#1a1a1a",
+      paper: "#2a2a2a",
+    },
+    text: {
+      primary: "#e0e0e0",
+      secondary: "#b0b0b0",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
